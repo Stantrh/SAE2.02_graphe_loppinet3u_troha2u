7c5885d3a98930aa79a51ef431f7dc23c8da8d8a
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-05-23 21:33:44 +0200

    Ajout de la javadoc complète dans Arc.java, Graphe.java, GrapheListe.java, Noeud.java

--- project/src/Graphe.java
+++ project/src/Graphe.java
@@ -1,14 +1,18 @@
 import java.util.List;
+
+/**
+ * Interface représentant un Graphe avec ses deux méthodes
+ */
 public interface Graphe {
 
 
-    /*
+    /**
     Retourne une liste de String qui correspond au noms des noeuds du graphe
     @return List<String> Liste de noms de noeuds
      */
     public List<String> listeNoeuds();
 
-    /*
+    /**
     Retourne une liste qui contient tous les Arcs partant du noeud qui a pour nom
     n passé en paramètres
     @return List<String> Liste d'Arcs
--- project/src/GrapheListe.java
+++ project/src/GrapheListe.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class GrapheListe implements Graphe{
+
     /**
     Correspond à une liste qui contient les noms des objets noeuds stockés
      */
@@ -24,7 +25,11 @@ public class GrapheListe implements Graphe{
         this.ensNoeuds = new ArrayList<Noeud>();
     }
 
-
+    /**
+     * Constructeur d'un graphe à partir du nom d'un fichier. On lit le fichier ligne par ligne
+     * Pour ensuite créer un arc avec chaque ligne puisqu'on a le cout, le noeud de départ et celui de destination
+     * @param nomFichierSrc nom du fichier source dont on veut extraire les données du graphe
+     */
     public GrapheListe(String nomFichierSrc){
         // Initialisation des listes dans tous les cas pour éviter de provoquer des erreurs à l'avenir (méthode add notamment)
         this.ensNom = new ArrayList<String>();
@@ -50,9 +55,10 @@ public class GrapheListe implements Graphe{
     }
 
     /**
-     * Getter qui retourne la liste des noeuds.
+     * Retourne la liste des noeuds en tant qu'objets Noeud
      * Utile pour les tests mais également lorsqu'on souhaite accéder
      * aux noeuds adjacents d'un noeud de la liste
+     * @return ensNoeud liste des Noeuds (en Noeud)
      */
     public List<Noeud> getListeNoeuds(){
         return this.ensNoeuds;
@@ -61,7 +67,7 @@ public class GrapheListe implements Graphe{
 
     /**
      * Retourne une liste de String comportant tous les noeuds du graphe (pas forcément dans l'ordre)
-     * @return List<String>
+     * @return la liste de Noeuds (en chaines de caractères)
      */
     public List<String> listeNoeuds(){
         return this.ensNom;
@@ -71,8 +77,8 @@ public class GrapheListe implements Graphe{
     Retourne la liste des Arcs d'un noeud dont le nom est passé en paramètres
      Si le noeud dont le nom est passé en paramètres n'est pas trouvé dans le graphe
      Alors on retourne une liste vide grâce à la classe Collection
-     @param n String qui correspond au nom du noeud dont on veut les arcs
-     @return List<Arc> </Arc>
+     @param n nom du noeud dont on veut les arcs
+     @return la liste des Arcs d'un noeud
      */
     public List<Arc> suivants(String n){
         int indice = this.ensNoeuds.indexOf(n);
@@ -88,38 +94,46 @@ public class GrapheListe implements Graphe{
      * et le nom du noeud de destination dont le nom est également donné en paramètre. Ainsi que le cout
      * Si les noeuds n'existent pas alors on les crée
      * Puis on ajoute l'arc
-     * @param depart
-     * @param destination
-     * @param cout
+     * @param depart noeud de départ
+     * @param destination noeud de destination de l'arc
+     * @param cout cout de l'arc reliant le noeud de départ à celui de destination
      */
     public void ajouterArc(String depart, String destination, double cout){
-        // Vérifier d'abord si depart existe bien
+        // Vérifier d'abord si le noeud de depart existe bien
         if(this.ensNom.indexOf(depart) != -1){
-            // Mais destination n'existe pas
+            // Mais si jamais le noeud de destination n'existe pas
             if(this.ensNom.indexOf(destination) == -1){
+                // Alors on l'ajoute aux deux listes
                 Noeud nDest = new Noeud(destination);
                 this.ensNoeuds.add(nDest);
                 this.ensNom.add(destination);
             }
+            // Puis dans tous les cas on ajoute l'arc au noeud de départ.
             this.ensNoeuds.get(this.ensNom.indexOf(depart)).ajouterArc(destination, cout);
         }else{
-            int indiceDestination = this.ensNom.indexOf(destination);
+            // Nous sommes dans le cas où le noeud de départ n'existe pas
+            // Donc on le crée et l'ajoute au deux listes
             Noeud nDepart = new Noeud(depart);
             this.ensNoeuds.add(nDepart);
             this.ensNom.add(depart);
             //Si le noeud de destination n'existe pas
-            if(indiceDestination == -1 ){
+            if(this.ensNom.indexOf(destination) == -1 ){
+                // On l'ajoute aux deux listes
                 Noeud nDest = new Noeud(destination);
                 this.ensNoeuds.add(nDest);
                 this.ensNom.add(destination);
             }
-
+            // Puis, que le noeud de destination existe ou pas, on peut ajouter l'arc entre les deux
             nDepart.ajouterArc(destination, cout);
 
         }
     }
 
-
+    /**
+     * Retourne une chaine de caractère qui correspond à la représentation du graphe en
+     * parcourant sa liste de noeuds
+     * @return affichage du graphe
+     */
     public String toString(){
         String aff = "";
         for(int i = 0; i < this.ensNom.size(); i++){
@@ -141,8 +155,15 @@ public class GrapheListe implements Graphe{
     }
 
 
+    /**
+     * Retourne un String qui correspond à l'affichage pour visualiser le graphe (sur plantUML ou
+     * https://dreampuf.github.io/GraphvizOnline/)
+     * @return aff
+     */
     public String toGraphviz(){
         String aff = "digraph G{\n";
+        // On parcourt la liste de Noeuds (contenant leurs noms uniquement)
+        // Puis on va ensuite parcourir la liste des noeuds adjaçants au noeud actuel et les afficher
         for(String s : this.ensNom){
             int indice = this.ensNom.indexOf(s);
             Noeud n = this.ensNoeuds.get(indice);
@@ -154,7 +175,14 @@ public class GrapheListe implements Graphe{
         return aff;
     }
 
-
+    /**
+     * Crée un nouveau fichier à partir d'un fichier sous forme de matrice représentant les relations entre
+     * chaque noeud. Cela donne un fichier qui lui peut être interprété par le constructeur si on lui passe son
+     * nom en paramètre.
+     * On part du principe que le fichier contenant la matrice est écrit sans erreurs.
+     * @param nomFichierSrc fichier source dont on veut extraire les arcs (représentés sous forme de matrice)
+     * @param nomFichierDest fichier de destination qui comportera un affichage tel que (noeudDépart NoeudDestination Cout)
+     */
     public void creerFichier(String nomFichierSrc, String nomFichierDest){
         try{
             // Pour pouvoir lire le fichier on crée un BufferedReader qui va le lire ligne par ligne
@@ -168,30 +196,31 @@ public class GrapheListe implements Graphe{
             // correspond à quel noeud
             String[] ligneUn = ligne.split("\t");
             ligne = fichier.readLine();
+
             // On parcourt a partir de la 2eme ligne puisqu'on veut créer
             // les noeuds adjacents pour chaque noeuds (écrits à partir de la 2eme ligne)
             while(ligne != null){
                 // Ici on a donc la ligne dans un tableau chaque
                 // terme du tableau étant une colonne
                 String[] colonnes = ligne.split("\t");
+
                 // On parcourt la ligne qui contient les noeuds
                 // adjacents au noeud
                 for(int i = 1; i < colonnes.length; i++){
                     if(!colonnes[i].equals("0.")){
-                        System.out.println(colonnes[0] + ligneUn[i] + colonnes[i]);
-                        String aff = colonnes[0] + " " + ligneUn[i] + " " + colonnes[i];
-                        fich.write(aff);
+                        fich.write(colonnes[0] + " " + ligneUn[i] + " " + colonnes[i]);
                         fich.newLine();
                     }
                 }
-
                 ligne = fichier.readLine();
             }
+            // On ferme les Buffers pour que la lecture et l'écriture de flux soient bien finis.
             fichier.close();
             fich.close();
-
+        // On gère les exceptions soit si le fichier source n'est pas trouvé
+            // Soit s'il y a une erreur de flux de lecture/écriture différente
         }catch(FileNotFoundException e){
-            System.err.println("Le(s) chemin(s) vers le(s) fichier(s) est incorrect");
+            System.err.println("Le chemin vers le fichier est incorrect");
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -200,10 +229,10 @@ public class GrapheListe implements Graphe{
     }
 
 
-
-
     public static void main(String[] args){
 
+
+        // On crée notre objet GrapheListe
         GrapheListe gL = new GrapheListe();
 
         // Ajout des arcs entre les noeuds
@@ -221,7 +250,8 @@ public class GrapheListe implements Graphe{
 
         gL.ajouterArc("E", "D", 43);
 
-        //System.out.println(gL.toGraphviz());
+
+        System.out.println(gL.toGraphviz());
 
         gL.creerFichier("ressources/matrice.txt", "ressources/output.txt");
 
