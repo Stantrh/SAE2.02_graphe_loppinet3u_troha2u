31cf64c57f108d20d5bb44cd2a28b0ca696830ab
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-05-25 19:09:29 +0200

    Remise à niveau d'à peu près tout

--- project/Test/MainComparaison.java
+++ project/Test/MainComparaison.java
@@ -2,28 +2,40 @@ public class MainComparaison {
 
     public static void main(String[] args){
 
-        GrapheListe gL = new GrapheListe("./ressources/fichier.txt");
+        // Graphe fourni en exemple sur Arche
+        GrapheListe grapheBoucle = new GrapheListe("./ressources/Graphe_boucle.txt");
+
+        // Graphe dans le dossier Graphe qui est très dense
+        GrapheListe grapheDense = new GrapheListe("./ressources/testGraphe.txt");
+        // Graphe21 Graphe34 Graphe61
         BellmanFord bF = new BellmanFord();
         Dijkstra d = new Dijkstra();
 
 
 
         System.out.println("Méthode BellmanFord");
-        long debutBF = System.nanoTime();
-        Valeur v1 = bF.resoudre(gL, "A");
-        long finBF = System.nanoTime();
-        long resultBF = finBF - debutBF;
-
-        System.out.println("Résolu en : " + finBF / 1000000000 + " secondes / " + finBF / 1000000 + " millisecondes.");
+        double debutBF = System.nanoTime();
+        Valeur v1 = bF.resoudre(grapheDense, "1");
+        double finBF = System.nanoTime();
+        double resultBF = finBF - debutBF;
 
 
         System.out.println("Méthode Dijkstra");
-        long debutD = System.nanoTime();
-        Valeur v2 = d.resoudre(gL, "A");
-        long finD = System.nanoTime();
-        long resultD = finD - debutD;
+        double debutD = System.nanoTime();
+        Valeur v2 = d.resoudre(grapheDense, "1");
+        double finD = System.nanoTime();
+        double resultD = finD - debutD;
 
-        System.out.println("Résolu en : " + finD / 1000000000 + " secondes / " + finD / 1000000 + " millisecondes.");
 
+
+        System.out.println("Méthode BellmanFord, graphe résolu en : " + resultBF / 1000000000 + " secondes / " + resultBF / 1000000 + " millisecondes.");
+
+        System.out.println("Méthode Dijkstra, graphe résolu en : " + resultD / 1000000000 + " secondes / " + resultD / 1000000 + " millisecondes.");
+
+        if(resultD < resultBF) {
+            System.out.println("Méthode Dijkstra plus rapide");
+        }else{
+            System.out.println("Méthode BellmanFord plus rapide");
+        }
     }
 }
